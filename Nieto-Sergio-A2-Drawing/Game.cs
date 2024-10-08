687851e10f24a9239fdb1f8aa32425b1b075f4cf
Shaft0509 <sergionieto555@gmail.com>
2024-10-08 16:09:44 -0500

    Agregar archivos de proyecto.

---
+++ Nieto-Sergio-A2-Drawing/Game.cs
@@ -0,0 +1,25 @@
+﻿
+using System;
+using System.Numerics;
+
+
+namespace Game10003
+{
+   
+    public class Game
+    {
+        
+        public void Setup()
+        {
+            Window.SetTitle("Spongey freak and Freaky Star");
+            Window.SetSize(800, 600);
+        }
+
+    
+        public void Update()
+        {
+            Window.ClearBackground(Color.Blue);
+
+        }
+    }
+}
